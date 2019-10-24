25f13c3577f2801c7b0d47b0613a87ca09f4a72f
ayogatot <azerino25@gmail.com>
2019-10-24 15:55:35 +0700

    Draggable v1

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,45 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Dragable Widget'),
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Draggable<Color>(
+                  data: color1,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: color1,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: color1.withOpacity(0.5),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
