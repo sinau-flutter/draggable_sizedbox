70a6c4ec38361db57d671a77ec666392cbbdc622
ayogatot <azerino25@gmail.com>
2019-10-30 15:18:36 +0700

    Add new SizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,35 @@ class _MyAppState extends State<MyApp> {
                       elevation: 3,
                     ),
                   ),
+                ),
+                Draggable<Color>(
+                  data: color2,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: color2,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.grey,
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: color2.withOpacity(0.5),
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
                 )
               ],
             ),
